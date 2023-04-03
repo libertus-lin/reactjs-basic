dbc2c22f75f95e14e6432e12a52abde82377bdd6
liebertuslin <codelinkursus@gmail.com>
2023-04-04 01:40:48 +0700

    first react learning

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className={'bg-blue-600 grid place-content-center text-white min-h-screen'}>
+      <h1 className={'text-xl'}>learn react</h1>
+    </div>
+  );
+}
